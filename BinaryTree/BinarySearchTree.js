c0d068fd076931e66ec4c35c20121e21fbf7db1c
feb <feb@sample.com>
2022-02-19 16:33:18 +0530

    added new dsa

--- BinaryTree/BinarySearchTree.js
+++ BinaryTree/BinarySearchTree.js
@@ -5,7 +5,7 @@ class BinarySearchTree {
     this.right = null;
   }
 
-  addNode(data) {git remote add origin https://github.com/febv1/DSA1.git
+  addNode(data) {
     if (data === this.data) {
       return;
     }
