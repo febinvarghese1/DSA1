dd966368d860f6cd93b444ba5dbe13a2e67dc0bd
febv1 <febv183@gmail.com>
2022-02-19 21:25:31 +0530

    added plaindrome method

--- Stacks/Stack.js
+++ Stacks/Stack.js
@@ -1,29 +1,38 @@
-class Stack{
-    constructor(){
-        this.buffer = [];
-    }
+class Stack {
+  constructor() {
+    this.buffer = [];
+  }
 
-    push(data){
-        this.buffer.push(data);
-    }
-    pop(){
-        return this.buffer.pop();
-    }
-    checkLength(){
-        return this.buffer.length;
-    }
-    isEmpty(){
-        return this.checkLength === 0;
-    }
-    printElements(){
-        this.buffer.forEach(elements=>console.log(elements))
-    }
+  push(data) {
+    this.buffer.push(data);
+  }
+  pop() {
+    return this.buffer.pop();
+  }
+  checkLength() {
+    return this.buffer.length;
+  }
+  isEmpty() {
+    return this.checkLength === 0;
+  }
+  printElements() {
+    this.buffer.forEach((elements) => console.log(elements));
+  }
 }
 
 const s = new Stack();
-s.push(1);
-s.push(2);
-s.push(3);
-s.push(4);
-s.push(5);
-s.printElements()
+
+const checkPlaindrome = (string) => {
+  for (let i = 0; i < string.length; i++) {
+    s.push(string[i]);
+  }
+
+  let rev_str = "";
+  for (let i = 0; i < string.length; i++) {
+    rev_str += s.pop();
+  }
+  return string == rev_str
+};
+
+console.log(checkPlaindrome("Hello"));
+console.log(checkPlaindrome("racecar"));
